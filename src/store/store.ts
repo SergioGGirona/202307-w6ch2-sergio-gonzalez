0f342a4b62e949b5ae2eb2f766bf705ccf345823
Sergio G. Girona <sergioglez_8@hotmail.com>
2023-08-23 08:45:58 +0200

    Fix develop problem

--- src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { counterReducer } from '../components/reducer/counter.reducer';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    tasks: () => ({}),
-  },
-});
