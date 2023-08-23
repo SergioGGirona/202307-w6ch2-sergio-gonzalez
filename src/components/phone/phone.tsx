72a68fa69340742a44c4bda109902392f9fd7415
Sergio G. Girona <sergioglez_8@hotmail.com>
2023-08-23 10:54:40 +0200

    Change value names

--- src/components/phone/phone.tsx
+++ src/components/phone/phone.tsx
@@ -3,9 +3,9 @@ import { Keyboard } from '../keyboard/keyboard';
 
 export function Phone() {
   return (
-    <>
+    <main>
       <Keyboard></Keyboard>
       <Actions></Actions>
-    </>
+    </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 import { appStore } from './components/store/store';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('container')!).render(
   <React.StrictMode>
     <Provider store={appStore}>
       <App />
